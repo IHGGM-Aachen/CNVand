300dffa62a6b9d21208f6fb991e24bb83087b51d
CarlosClassen <carlos.classen@rwth-aachen.de>
2024-06-03 00:05:59 +0100

    Add unit tests

---
+++ .tests/unit/test_cnvkit_antitarget_coverage.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+import subprocess as sp
+from tempfile import TemporaryDirectory
+import shutil
+from pathlib import Path, PurePosixPath
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+import common
+
+
+def test_cnvkit_antitarget_coverage():
+
+    with TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        data_path = PurePosixPath(".tests/unit/cnvkit_antitarget_coverage/data")
+        expected_path = PurePosixPath(".tests/unit/cnvkit_antitarget_coverage/expected")
+
+        # Copy data to the temporary workdir.
+        shutil.copytree(data_path, workdir)
+
+        # dbg
+        print("output/cnv/M24352/M24352.antitargetcoverage.cnn", file=sys.stderr)
+
+        # Run the test job.
+        sp.check_output([
+            "python",
+            "-m",
+            "snakemake", 
+            "output/cnv/M24352/M24352.antitargetcoverage.cnn",
+            "-f", 
+            "-j1",
+            "--target-files-omit-workdir-adjustment",
+    
+            "--use-conda",
+            "--directory",
+            workdir,
+        ])
+
+        # Check the output byte by byte using cmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # also see common.py.
+        common.OutputChecker(data_path, expected_path, workdir).check()
---
+++ .tests/unit/test_samtools_sort.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+import subprocess as sp
+from tempfile import TemporaryDirectory
+import shutil
+from pathlib import Path, PurePosixPath
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+import common
+
+
+def test_samtools_sort():
+
+    with TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        data_path = PurePosixPath(".tests/unit/samtools_sort/data")
+        expected_path = PurePosixPath(".tests/unit/samtools_sort/expected")
+
+        # Copy data to the temporary workdir.
+        shutil.copytree(data_path, workdir)
+
+        # dbg
+        print("output/preprocessing/M24352.sorted.bam", file=sys.stderr)
+
+        # Run the test job.
+        sp.check_output([
+            "python",
+            "-m",
+            "snakemake", 
+            "output/preprocessing/M24352.sorted.bam",
+            "-f", 
+            "-j1",
+            "--target-files-omit-workdir-adjustment",
+    
+            "--use-conda",
+            "--directory",
+            workdir,
+        ])
+
+        # Check the output byte by byte using cmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # also see common.py.
+        common.OutputChecker(data_path, expected_path, workdir).check()
